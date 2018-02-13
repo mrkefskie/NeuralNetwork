5522ab426cb697f4aa89ce0d7ad32f54e3e89868
Kevin Slagmolen <mrkefskie@gmail.com>
2018-02-13 12:18:41 +0100

    Worked on implementation for SimpleNetwork

--- NeuralNetwork/NeuralNetwork/SimpleNetwork.cpp
+++ NeuralNetwork/NeuralNetwork/SimpleNetwork.cpp
@@ -1,5 +1,7 @@
 #include "SimpleNetwork.h"
 
+#define DEBUG
+
 NeuralNetwork::SimpleNetwork::SimpleNetwork(unsigned int input_nodes, unsigned int hidden_nodes, unsigned int output_nodes) :
 	_input_nodes(input_nodes), _hidden_nodes(hidden_nodes), _output_nodes(output_nodes)
 {
@@ -40,83 +42,133 @@ float* NeuralNetwork::SimpleNetwork::predict(float* input_data)
 {
 	Utilities::Matrix inputs = Utilities::Matrix::fromArray(input_data, _input_nodes);
 
-	Utilities::Matrix hidden = _weights_ih * inputs;
-	hidden += _bias_h;
+	Utilities::Matrix hidden_inputs = _weights_ih * inputs;
+	Utilities::Matrix hidden_outputs(hidden_inputs.getRows(), hidden_inputs.getCols());
 
-	for (unsigned long i = 0; i < hidden.getRows(); i++)
+	for (unsigned long i = 0; i < hidden_inputs.getRows(); i++)
 	{
-		hidden.setData(i, 0, ActivationFunctions::activate(SIGMOID_ACTIVATION, hidden.getData(i, 0)));
+		hidden_outputs.setData(i, 0, ActivationFunctions::activate(SIGMOID_ACTIVATION, hidden_inputs.getData(i, 0)));
 	}
 
+	Utilities::Matrix final_inputs = _weights_ho * hidden_outputs;
+	Utilities::Matrix final_outputs(final_inputs.getRows(), final_inputs.getCols());
 
-	Utilities::Matrix output = _weights_ho * hidden;
-	output += _bias_o;
-
-	for (unsigned long i = 0; i < hidden.getRows(); i++)
+	for (unsigned long i = 0; i < final_inputs.getRows(); i++)
 	{
-		output.setData(i, 0, ActivationFunctions::activate(SIGMOID_ACTIVATION, output.getData(i, 0)));
+		final_outputs.setData(i, 0, ActivationFunctions::activate(SIGMOID_ACTIVATION, final_inputs.getData(i, 0)));
 	}
 
-	return output.toArray();
+	return final_outputs.toArray();
 }
 
 void NeuralNetwork::SimpleNetwork::train(float * input_data, float * output_data)
 {
 	Utilities::Matrix inputs = Utilities::Matrix::fromArray(input_data, _input_nodes);
+	Utilities::Matrix targets = Utilities::Matrix::fromArray(output_data, _output_nodes);
+
+	Utilities::Matrix hidden_inputs = _weights_ih * inputs;
+	Utilities::Matrix hidden_outputs(hidden_inputs.getRows(), hidden_inputs.getCols());
+
+	for (unsigned long i = 0; i < hidden_inputs.getRows(); i++)
+	{
+		hidden_outputs.setData(i, 0, ActivationFunctions::activate(SIGMOID_ACTIVATION, hidden_inputs.getData(i, 0)));
+	}
+
+	Utilities::Matrix final_inputs = _weights_ho * hidden_outputs;
+	Utilities::Matrix final_outputs(final_inputs.getRows(), final_inputs.getCols());
+
+	for (unsigned long i = 0; i < final_inputs.getRows(); i++)
+	{
+		final_outputs.setData(i, 0, ActivationFunctions::activate(SIGMOID_ACTIVATION, final_inputs.getData(i, 0)));
+	}
+
+	// Output layer error is the (target - actual)
+	Utilities::Matrix output_errors = targets - final_outputs;
+
+	// Hidden layer error is the output_errors, split by weights, recombined at hidden nodes
+	Utilities::Matrix hidden_errors = _weights_ho.transpose() * output_errors;
+
+	// Update the weights for the links between the hidden and output layers
+	Utilities::Matrix final_outputs_back(final_outputs.getRows(), final_outputs.getCols());
+	for (unsigned long i = 0; i < final_outputs_back.getRows(); i++)
+	{
+		final_outputs_back.setData(i, 0, ActivationFunctions::deActivate(SIGMOID_ACTIVATION, final_outputs.getData(i, 0)));
+	}
+
+	Utilities::Matrix tmp = output_errors * final_outputs_back;
+	tmp = tmp * _learningRate;
+	_weights_ho = _weights_ho + tmp;
+
+	
+
+
+
+	/*// Generate the hidden outputs
+	Utilities::Matrix inputs = Utilities::Matrix::fromArray(input_data, _input_nodes);
 
 	Utilities::Matrix hidden = _weights_ih * inputs;
-	hidden += _bias_h;
-
+	hidden += _bias_h+0;
+	// Activation Functions
 	for (unsigned long i = 0; i < hidden.getRows(); i++)
 	{
 		hidden.setData(i, 0, ActivationFunctions::activate(SIGMOID_ACTIVATION, hidden.getData(i, 0)));
 	}
 
-
+	// Geneate the output's output
 	Utilities::Matrix outputs = _weights_ho * hidden;
 	outputs += _bias_o;
-
-	for (unsigned long i = 0; i < hidden.getRows(); i++)
+	// Activation Functions
+	for (unsigned long i = 0; i < outputs.getRows(); i++)
 	{
 		outputs.setData(i, 0, ActivationFunctions::activate(SIGMOID_ACTIVATION, outputs.getData(i, 0)));
 	}
 
-	
+	// Convert array to Matrix
 	Utilities::Matrix targets = Utilities::Matrix::fromArray(output_data, 1);
 
+
+	// Calculate the errors (targets-outputs)
 	Utilities::Matrix output_errors = targets - outputs;
 
-	Utilities::Matrix gradients = outputs;
+	// Deactivation (x = y * (1 - y))
+	Utilities::Matrix gradients = outputs+0;
 	for (unsigned long i = 0; i < gradients.getRows(); i++)
 	{
-		gradients.setData(i, 0, ActivationFunctions::deActivate(SIGMOID_ACTIVATION, outputs.getData(i, 0)));
+		gradients.setData(i, 0, ActivationFunctions::deActivate(SIGMOID_ACTIVATION, gradients.getData(i, 0)));
 	}
 
+	// Calculate gradients
 	gradients = gradients * output_errors;
 	gradients = gradients * _learningRate;
 
-	Utilities::Matrix hidden_T = Utilities::Matrix::transpose(inputs);
+	// Calculate deltas
+	Utilities::Matrix hidden_T = hidden.transpose();
 	Utilities::Matrix weight_ho_deltas = gradients * hidden_T;
 
+	// Adjust weights by deltas
 	_weights_ho += weight_ho_deltas;
+	// Adjust bias by its deltas (gradient)
 	_bias_o += gradients;
 
 
-	Utilities::Matrix who_T = Utilities::Matrix::transpose(_weights_ho);
-	Utilities::Matrix hidden_errors = who_T * output_errors;
+	// Calculate the hidden layer error
+	Utilities::Matrix who_T = _weights_ho.transpose();
+	Utilities::Matrix hidden_errors = (who_T * output_errors).transpose();
 
-	Utilities::Matrix hidden_gradient = hidden;
-	for (unsigned long i = 0; i < gradients.getRows(); i++)
+	// Calculate hidden gradient
+	Utilities::Matrix hidden_gradient = hidden+0;
+	for (unsigned long i = 0; i < hidden_gradient.getRows(); i++)
 	{
-		hidden_gradient.setData(i, 0, ActivationFunctions::deActivate(SIGMOID_ACTIVATION, hidden.getData(i, 0)));
+		hidden_gradient.setData(i, 0, ActivationFunctions::deActivate(SIGMOID_ACTIVATION, hidden_gradient.getData(i, 0)));
 	}
 
 	hidden_gradient = hidden_gradient * hidden_errors;
 	hidden_gradient = hidden_gradient * _learningRate;
 
-	Utilities::Matrix inputs_T = Utilities::Matrix::transpose(inputs);
+	// Calculate input->hidden deltas
+	Utilities::Matrix inputs_T = inputs + 0;// .transpose();
 	Utilities::Matrix weights_ih_deltas = hidden_gradient * inputs_T;
 
-	_weights_ih += weights_ih_deltas;
-	_bias_h += hidden_gradient;
+	_weights_ih += hidden_gradient;// weights_ih_deltas;
+	_bias_h += weights_ih_deltas;// hidden_gradient;*/
 }
--- NeuralNetwork/main.cpp
+++ NeuralNetwork/main.cpp
@@ -7,24 +7,74 @@
 #include "NeuralNetwork\Node.h"
 #include "NeuralNetwork\SimpleNetwork.h"
 
+#include "Data\Data.h"
+
 #include "GUI\SimpleScreen.h"
 
 #include <Matrix\Matrix.h>
 
 #define AMOUNT_OF_TRAINING_POINTS 10000
 
+
 int main(int argc, char** argv)
 {
 	srand(time(NULL));
 
-	NeuralNetwork::SimpleNetwork network(2, 4, 1);
+	Data::DATA_TYPE XOR[4];
+	for (int i = 0; i < 4; i++)
+	{
+		XOR[i].input_length = 2;
+		XOR[i].output_length = 1;
 
-	float* tst = (float*)malloc(sizeof(float) * 2);
-	tst[0] = 0.5f;
-	tst[1] = 0.6f;
+		XOR[i].inputs = (float*)malloc(sizeof(float) * XOR[i].input_length);
+		XOR[i].outputs = (float*)malloc(sizeof(float) * XOR[i].output_length);
+	}
 
-	float* prediction = network.predict(tst);
+	XOR[0].inputs[0] = 0;
+	XOR[0].inputs[1] = 0;
+	XOR[0].outputs[0] = 0;
+
+	XOR[1].inputs[0] = 0;
+	XOR[1].inputs[1] = 1;
+	XOR[1].outputs[0] = 1;
+
+	XOR[2].inputs[0] = 1;
+	XOR[2].inputs[1] = 0;
+	XOR[2].outputs[0] = 0;
+
+	XOR[3].inputs[0] = 1;
+	XOR[3].inputs[1] = 1;
+	XOR[3].outputs[0] = 0;
+
+	NeuralNetwork::SimpleNetwork network(2, 2, 1);
+
+	GUI::SimpleScreen screen;
+
+	float* prediction = network.predict(XOR[0].inputs);
 	printf("guess = %.2f\r\n", prediction[0]);
+
+	network.train(XOR[0].inputs, XOR[0].outputs);
+
+	/*for (unsigned long i = 0; i < 1000000; i++)
+	{
+		int random = rand() % (3 - 0 + 1) + 0;
+
+		network.train(XOR[random].inputs, XOR[random].outputs);
+
+		prediction = network.predict(XOR[0].inputs);
+		printf("iteration: %d\t\tguess = %.2f\r\n", i, prediction[0]);
+
+		if (cv::waitKey(1) == 27)
+		{
+			cv::destroyAllWindows();
+			break;
+		}
+		if (i % 100 == 0)
+			screen.render(network, XOR, 4);
+	}
+
+	prediction = network.predict(XOR[0].inputs);
+	printf("guess = %.2f\r\n", prediction[0]);*/
 
 	system("pause");
 	return 1;
@@ -40,7 +90,7 @@ int main(int argc, char** argv)
 		p[i] = Data::Point(Data::STRAIGHT);
 	}
 
-	GUI::SimpleScreen screen;
+	//GUI::SimpleScreen screen;
 
 	uint32_t counter = 0;
 
