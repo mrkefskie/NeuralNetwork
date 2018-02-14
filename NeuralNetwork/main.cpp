7c79858b485b8354b8b7973c6528441605e722bc
Kevin Slagmolen <mrkefskie@gmail.com>
2018-02-14 11:36:45 +0100

    Used define

--- NeuralNetwork/main.cpp
+++ NeuralNetwork/main.cpp
@@ -11,15 +11,14 @@
 
 #include "GUI\SimpleScreen.h"
 
-#define AMOUNT_OF_TRAINING_POINTS 10000
-
+#define AMOUNT_OF_TRAINING_POINTS 5
 
 int main(int argc, char** argv)
 {
 	srand(time(NULL));
 
-	Data::DATA_TYPE XOR[4];
-	for (int i = 0; i < 4; i++)
+	Data::DATA_TYPE XOR[AMOUNT_OF_TRAINING_POINTS];
+	for (int i = 0; i < AMOUNT_OF_TRAINING_POINTS; i++)
 	{
 		XOR[i].input_length = 2;
 		XOR[i].output_length = 1;
@@ -28,37 +27,38 @@ int main(int argc, char** argv)
 		XOR[i].outputs = (float*)malloc(sizeof(float) * XOR[i].output_length);
 	}
 
-	XOR[0].inputs[0] = 0;
-	XOR[0].inputs[1] = 0;
-	XOR[0].outputs[0] = 0;
+	XOR[0].inputs[0] = 0.0f;
+	XOR[0].inputs[1] = 0.0f;
+	XOR[0].outputs[0] = 0.0f;
 
-	XOR[1].inputs[0] = 0;
-	XOR[1].inputs[1] = 1;
-	XOR[1].outputs[0] = 1;
+	XOR[1].inputs[0] = 0.0f;
+	XOR[1].inputs[1] = 1.0f;
+	XOR[1].outputs[0] = 1.0f;
 
-	XOR[2].inputs[0] = 1;
-	XOR[2].inputs[1] = 0;
-	XOR[2].outputs[0] = 1;
+	XOR[2].inputs[0] = 1.0f;
+	XOR[2].inputs[1] = 0.0f;
+	XOR[2].outputs[0] = 1.0f;
 
-	XOR[3].inputs[0] = 1;
-	XOR[3].inputs[1] = 1;
-	XOR[3].outputs[0] = 0;
+	XOR[3].inputs[0] = 1.0f;
+	XOR[3].inputs[1] = 1.0f;
+	XOR[3].outputs[0] = 0.0f;
 
-	NeuralNetwork::SimpleNetwork network(2, 2, 1, 0.01);
+	XOR[4].inputs[0] = 0.5f;
+	XOR[4].inputs[1] = 0.5f;
+	XOR[4].outputs[0] = 0.0f;
+
+
+	NeuralNetwork::SimpleNetwork network(2, 10, 1, 1);
 
 	GUI::SimpleScreen screen;
 
-	float* prediction = network.predict(XOR[0].inputs);
-	//printf("guess = %.2f\r\n", prediction[0]);
-
-	//network.train(XOR[0].inputs, XOR[0].outputs);
 
 	for (unsigned long i = 0; i < 1000000; i++)
 	{
-		int random = rand() % (3 - 0 + 1) + 0;
-
-		network.train(XOR[random].inputs, XOR[random].outputs);
-
+		for (unsigned long j = 0; j < AMOUNT_OF_TRAINING_POINTS; j++)
+		{
+			network.train(XOR[j].inputs, XOR[j].outputs);
+		}
 		if (cv::waitKey(1) == 27)
 		{
 			cv::destroyAllWindows();
@@ -71,8 +71,11 @@ int main(int argc, char** argv)
 		}
 	}
 
-	prediction = network.predict(XOR[0].inputs);
-	printf("guess = %.2f\r\n", prediction[0]);
+	for (size_t i = 0; i < AMOUNT_OF_TRAINING_POINTS; i++)
+	{
+		float* prediction = network.predict(XOR[i].inputs);
+		printf("%.f and %.f = guess %.2f\t\tShould be %.2f\r\n", XOR[i].inputs[0], XOR[i].inputs[1], prediction[0], XOR[i].outputs[0]);
+	}
 
 	system("pause");
 	return 1;
